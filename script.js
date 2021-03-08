23c50e42044f99f65c2cff75a7e305dcaa5c0da6
Pavelkv94 <pavel.kv94@yandex.by>
2021-03-08 18:33:19 +0300

    feat: add change Name & Task

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //DOM Eelements
-const time = document.getElementById("time"),
-  greeting = document.getElementById("greeting"),
-  name = document.getElementById("name"),
-  focus = document.getElementById("focus");
+let time = document.getElementById("time");
+let greeting = document.getElementById("greeting");
+let name = document.getElementById("name");
+let focus = document.getElementById("focus");
 
-//Show time
+//Options
+const showAmPm = true;
+
+//TODO-functions Show time
 function showTime() {
   let today = new Date();
   let hour = today.getHours();
@@ -15,41 +18,89 @@ function showTime() {
   const amPm = hour >= 12 ? "PM" : "AM";
 
   // 12hr Format
-  //hour = hour % 12 || 12;
+  hour = hour % 12 || 12;
 
   //Output time
-  time.innerHTML = `${hour}<span>:</span> ${addZeros(
-    min
-  )}<span>:</span> ${addZeros(sec)}`;
+  time.innerHTML = `${hour}<span>:</span> 
+  ${addZeros(min)}<span>:</span> 
+  ${addZeros(sec)} ${showAmPm ? amPm : ""}`;
   setTimeout(showTime, 1000);
 }
-//Add Zeros
+//TODO-functions Zeros
 function addZeros(n) {
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
-//Set Background and Greeting
+//TODO-functions Background and Greeting
 function setBgGreet() {
   let today = new Date();
   let hour = today.getHours();
 
   if (hour < 12) {
     //Morning
-document.body.style.backgroundImage='url(./assets/images/morning/02.jpg)'
-greeting.textContent = "Good Morning"
-document.body.style.color = "white"
+    document.body.style.backgroundImage = "url(./assets/images/morning/02.jpg)";
+    greeting.textContent = "Good Morning";
+    document.body.style.color = "white";
   } else if (hour < 18) {
     //Afternoon
-    document.body.style.backgroundImage='url(./assets/images/morning/01.jpg)'
-    greeting.textContent = "Good Afternoon"
-    document.body.style.color = "white"
+    document.body.style.backgroundImage = "url(./assets/images/morning/01.jpg)";
+    greeting.textContent = "Good Afternoon";
+    document.body.style.color = "white";
   } else {
     //Evening
-    document.body.style.backgroundImage='url(./assets/images/morning/03.jpg)'
-    greeting.textContent = "Good Evening"
-    document.body.style.color = "white"
+    document.body.style.backgroundImage = "url(./assets/images/morning/08.jpg)";
+    greeting.textContent = "Good Evening";
+    document.body.style.color = "white";
   }
 }
 
-//Run
+//TODO-functions Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+//TODO-functions Set Name
+function setName(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+//TODO-functions Get Focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+//TODO-functions Set Focus
+function setFocus(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
+//!Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
